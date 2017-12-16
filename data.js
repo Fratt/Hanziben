5cab94b1ec01e16fef03e6ad247cee8b62d115f4
Christophe Frattino <christophe@frattino.ch>
2017-12-16 12:41:14 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,218 @@
+var hanziben =
+	[
+		{
+			lesson: "0",
+			words: [
+				{ fr: "un", zh: "", pinyin: ["yi1"] },
+				{ fr: "deux", zh: "", pinyin: ["er4"] },
+				{ fr: "trois", zh: "", pinyin: ["san1"] },
+				{ fr: "quatre", zh: "", pinyin: ["si4"] },
+				{ fr: "cinq", zh: "", pinyin: ["wu3"] },
+				{ fr: "six", zh: "", pinyin: ["liu4"] },
+				{ fr: "sept", zh: "", pinyin: ["qi1"] },
+				{ fr: "huit", zh: "", pinyin: ["ba1"] },
+				{ fr: "neuf", zh: "", pinyin: ["jiu3"] },
+				{ fr: "dix", zh: "", pinyin: ["shi2"] },
+				{ fr: "homme", zh: "", pinyin: ["ren2"] },
+				/*{ fr: "grand", pinyin: ["da4"] },*/
+				/*{ fr: "bouche", pinyin: ["kou3"] },*/
+				/*{ fr: "milieu", pinyin: ["zhong1"] },*/
+				/*{ fr: "petit", pinyin: ["xiao3"] },*/
+				{ fr: "dessus", zh: "", pinyin: ["shang4"] },
+				{ fr: "dessous", zh: "", pinyin: ["xia4"] },
+				/*{ fr: "pays", pinyin: ["guo2"] },*/
+				{ fr: "Chine", zh: "", pinyin: ["Zhong1", "guo2"] },
+				{ fr: "étudier, apprendre", zh: "", pinyin: ["xue2", "xi2"] },
+				/*{ fr: "langage (1)", pinyin: ["yu3"] },*/
+				/*{ fr: "langage (2)", pinyin: ["wen2"] },*/
+				{ fr: "écrire", zh: "", pinyin: ["xie3"] },
+				/*{ fr: "caractère", pinyin: ["zi4"] },*/
+				/*{ fr: "langue chinoise (1)", pinyin: ["Han4", "yu3"] },*/
+				{ fr: "langue chinoise", zh: "", pinyin: ["Zhong1", "wen2"] },
+				{ fr: "caractère chinois", zh: "", pinyin: ["Han4", "zi4"] },
+				/*{ fr: "femme", pinyin: ["nv3"] },*/
+				/*{ fr: "fils, enfant", pinyin: ["zi3"] },*/
+				{ fr: "bien, bon", zh: "", pinyin: ["hao3"] },
+				{ fr: "eau", zh: "", pinyin: ["shui3"] },
+				{ fr: "porte", zh: "", pinyin: ["men2"] },
+				{ fr: "roi", zh: "", pinyin: ["wang2"] },
+				/*{ fr: "femme, fille", pinyin: ["nv3", "zi3"] },*/
+				/*{ fr: "entrée", pinyin: ["men2", "kou3"] },*/
+				/*{ fr: "roi (2)", pinyin: ["guo2", "wang2"] },*/
+				{ fr: "soleil", zh: "", pinyin: ["ri4"] },
+				{ fr: "lune", zh: "", pinyin: ["yue4"] },
+				{ fr: "bois", zh: "", pinyin: ["mu4"] },
+				{ fr: "cheval", zh: "", pinyin: ["ma3"] },
+				{ fr: "champ", zh: "", pinyin: ["tian2"] },
+				{ fr: "ciel", zh: "", pinyin: ["tian1"] },
+				{ fr: "lumineux", zh: "", pinyin: ["ming2"] },
+				{ fr: "forêt", zh: "", pinyin: ["lin2"] },
+				{ fr: "mère", zh: "", pinyin: ["ma1"] },
+				{ fr: "rivière", zh: "", pinyin: ["he2"] }						
+			]
+		},
+		{
+			lesson: "1",
+			words: [
+				{ fr: "vous", zh: "", pinyin: ["nin2"] },
+				/*{ fr: "honorable", pinyin: ["gui4"] },*/
+				{ fr: "nom, s'appeller", zh: "", pinyin: ["xing4"] },
+				{ fr: "je, me, moi", zh: "", pinyin: ["wo3"] },
+				{ fr: "<i>particule<br/>interrogative 1</i>", zh: "", pinyin: ["ne"] },
+				{ fr: "être", zh: "", pinyin: ["shi4"] },
+				{ fr: "<i>particule<br/>interrogative 2</i>", zh: "", pinyin: ["ma"] },
+				{ fr: "non, ne pas", zh: "", pinyin: ["bu4"] },
+				{ fr: "camarde</br>de classe", zh: "", pinyin: ["tong2", "xue2"] },
+				{ fr: "<i>suffixe</i><br/>ou <i>pluriel</i>", zh: "", pinyin: ["men"] },
+				{ fr: "professeur", zh: "", pinyin: ["lao3", "shi1"] },
+				{ fr: "(s')appeler,<br/>nommer", zh: "", pinyin: ["jiao4"] },
+				{ fr: "tu, toi, te", zh: "", pinyin: ["ni3"] },
+				{ fr: "quoi, que, quel", zh: "", pinyin: ["shen2", "me"] },
+				{ fr: "nom", zh: "", pinyin: ["ming2", "zi"] },
+				{ fr: "quel", zh: "", pinyin: ["na3"] },
+				{ fr: "aussi,<br/>non plus", zh: "", pinyin: ["ye3"] },
+				{ fr: "dire,<br/>parler", zh: "", pinyin: ["shuo1"] },
+				{ fr: "langue anglaise", zh: "", pinyin: ["ying1", "yu3"] },
+				{ fr: "ou", zh: "", pinyin: ["hai2", "shi4"] },
+				{ fr: "langue française", zh: "", pinyin: ["fa3", "yu3"] },
+				{ fr: "tout, tous", zh: "", pinyin: ["dou1"] },
+				{ fr: "il, lui", zh: "", pinyin: ["ta1"] },
+				{ fr: "elle", zh: "", pinyin: ["ta1"] },
+				{ fr: "seulement,<br/>uniquement", zh: "", pinyin: ["zhi3"] }
+			]
+		},
+		{
+			lesson: "2",
+			words: [
+				{ fr: "connaître", zh: "", pinyin: ["ren4", "shi"] },
+				{ fr: "très", zh: "", pinyin: ["hen3"] },
+				{ fr: "heureux, content", zh: "", pinyin: ["gao1", "xing4"] },
+				{ fr: "(être) dans,<br/>en, sur...", zh: "", pinyin: ["zai4"] },
+				{ fr: "import-export", zh: "", pinyin: ["jin4", "chu1", "kou3"] },
+				{ fr: "entreprise", zh: "", pinyin: ["gong1", "si1"] },
+				{ fr: "travailler, travail", zh: "", pinyin: ["gong1", "zuo4"] },
+				{ fr: "université", zh: "", pinyin: ["da4", "xue2"] },
+				{ fr: "département", zh: "", pinyin: ["xi4"] },
+				{ fr: "pouvoir", zh: "", pinyin: ["ke3", "yi3"] },
+				{ fr: "donner, à,<br/>pour", zh: "", pinyin: ["gei3"] },
+				{ fr: "téléphoner", zh: "", pinyin: ["da3", "dian4", "hua4"] },
+				{ fr: "mon", zh: "", pinyin: ["wo3", "de"] },
+				{ fr: "numéro", zh: "", pinyin: ["hao4", "ma3"] },
+				{ fr: "envoyer,<br/>poster", zh: "", pinyin: ["fa1"] },
+				{ fr: "adresse e-mail", zh: "", pinyin: ["dian4", "zi3", "you2", "jian4"] },
+				{ fr: "ce, cet,<br/>cette", zh: "", pinyin: ["zhe4"] },
+				{ fr: "féminin", zh: "", pinyin: ["nv3"] },
+				{ fr: "ami", zh: "", pinyin: ["peng2", "you"] },
+				{ fr: "SVP,<br/>prier de", zh: "", pinyin: ["qing3"] },
+				{ fr: "s'asseoir", zh: "", pinyin: ["zuo4"] },
+				{ fr: "merci", zh: "", pinyin: ["xie4", "xie"] },
+				{ fr: "boire", zh: "", pinyin: ["he1"] },
+				{ fr: "thé", zh: "", pinyin: ["cha2"] },
+				{ fr: "où", zh: "", pinyin: ["nar3",""] },
+				{ fr: "institut", zh: "", pinyin: ["xue2","yuan4"] },
+				{ fr: "comment (avis)", zh: "", pinyin: ["zen3", "me", "yang4"] },
+				{ fr: "joli, beau", zh: "", pinyin: ["piao4", "liang"] },
+				{ fr: "aimer", zh: "", pinyin: ["xi3", "huan"] },
+				{ fr: "ce, cela", zh: "", pinyin: ["na4"] },
+				{ fr: "masculin", zh: "", pinyin: ["nan2"] },
+				{ fr: "petit", zh: "", pinyin: ["xiao3"] }
+			]
+		},
+		{
+			lesson: "3",
+			words: [
+				{ fr: "endroit", zh: "", pinyin: ["di4", "fang"] },
+				{ fr: "spécificatif", zh: "", pinyin: ["ge4"] },
+				{ fr: "maison, famille", zh: "", pinyin: ["jia1"] },
+				{ fr: "avoir", zh: "", pinyin: ["you3"] },
+				{ fr: "combien", zh: "", pinyin: ["ji3"] },
+				{ fr: "père, papa", zh: "", pinyin: ["ba4", "ba"] },
+				{ fr: "et", zh: "", pinyin: ["he2"] },
+				{ fr: "épouse", zh: "", pinyin: ["tai4", "tai"] },
+				{ fr: "enfant", zh: "", pinyin: ["hai2", "zi"] },
+				{ fr: "combien", zh: "", pinyin: ["duo1"] },
+				{ fr: "deux", zh: "", pinyin: ["liang3"] },
+				{ fr: "âge", zh: "", pinyin: ["sui4"] },
+				{ fr: "aimer", zh: "", pinyin: ["ai4"] },
+				{ fr: "particule", zh: "", pinyin: ["ba"] },
+				{ fr: "école", zh: "", pinyin: ["xue2", "xiao4"] },
+				{ fr: "peu", zh: "", pinyin: ["shao3"] },
+				{ fr: "étudiant", zh: "", pinyin: ["xue2", "sheng(1)"] },
+				{ fr: "penser, vouloir<br/>faire qch.", zh: "", pinyin: ["xiang3"] },
+				{ fr: "peut-être,<br/>environ", zh: "", pinyin: ["da4", "gai4"] },
+				{ fr: "10'000", zh: "", pinyin: ["wan4"] },
+				{ fr: "ne pas", zh: "", pinyin: ["mei2"] },
+				{ fr: "1'000", zh: "", pinyin: ["qian1"] },
+				{ fr: "aller", zh: "", pinyin: ["qu4"] },
+				{ fr: "branche", zh: "", pinyin: ["fen1"] },
+				{ fr: "boss", zh: "", pinyin: ["lao3", "ban3"] },
+				{ fr: "laisser,<br/>permettre", zh: "", pinyin: ["rang4"] },
+				{ fr: "100", zh: "", pinyin: ["bai3"] },
+				{ fr: "parce que,<br/>car", zh: "", pinyin: ["yin1", "wei4"] }						
+			]
+		},
+		{
+			lesson: "4 (partie 1)",
+			words: [
+				{ fr: "carte", zh: "", pinyin: ["di4", "tu2"] },
+				{ fr: "spécif. (papier)", zh: "", pinyin: ["zhang1"] },
+				{ fr: "regarder", zh: "", pinyin: ["kan4"] },
+				{ fr: "d'accord", zh: "", pinyin: ["xing4"] },
+				{ fr: "vouloir", zh: "", pinyin: ["yao4"] },
+				{ fr: "faire", zh: "", pinyin: ["gan4"] },
+				{ fr: "jouer", zh: "", pinyin: ["wan2"] },
+				{ fr: "savoir", zh: "", pinyin: ["zhi1", "dao4"] },
+				{ fr: "relativement", zh: "", pinyin: ["bi3", "jiao4"] },
+				{ fr: "réellement", zh: "", pinyin: ["zhen1"] },
+				{ fr: "signification", zh: "", pinyin: ["yi4", "si1"] },
+				{ fr: "dragon", zh: "", pinyin: ["long2"] },
+				{ fr: "montagne", zh: "", pinyin: ["shan1"] },
+			]
+		},
+		{
+			lesson: "5",
+			checked: true,
+			words: [
+				{ fr: "mademoiselle", zh: "小姐", pinyin: ["xiao3", "jie3"] },
+				{ fr: "savoir faire qqch", zh: "会", pinyin: ["hui4"] },
+				{ fr: "un peu", zh: "一点儿", pinyin: ["yi4", "dianr3", ""] },
+				{ fr: "monsieur", zh: "先生", pinyin: ["xian1", "sheng1"] },
+				{ fr: "acheter", zh: "买", pinyin: ["mai3"] },
+				{ fr: "chemise", zh: "衬衫", pinyin: ["chen4", "shan1"] },
+				{ fr: "blanc", zh: "白", pinyin: ["bai2"] },
+				{ fr: "argent", zh: "钱", pinyin: ["qian2"] },
+				{ fr: "Renminbi", zh: "块", pinyin: ["kuai4"] },
+				{ fr: "Yuan", zh: "元", pinyin: ["yuan2"] },
+				{ fr: "(passé)", zh: "了", pinyin: ["le"] },
+				{ fr: "cher", zh: "贵", pinyin: ["gui4"] },
+				{ fr: "rouge", zh: "红", pinyin: ["hong2"] },
+				{ fr: "bon marché", zh: "便宜", pinyin: ["pian2", "yi4"] },
+				{ fr: "spécif.<br/>(pantalons)", zh: "条", pinyin: ["tiao4"] },
+				{ fr: "pantalon", zh: "裤子", pinyin: ["ku4", "zi3"] },
+				{ fr: "essayer", zh: "试", pinyin: ["shi4"] },
+				{ fr: "venir", zh: "来", pinyin: ["lai2"] },
+				{ fr: "restaurant", zh: "饭店", pinyin: ["fan4", "dian4"] },
+				{ fr: "le plus", zh: "最", pinyin: ["zui4"] },
+				{ fr: "manger", zh: "吃", pinyin: ["chi1"] },
+				{ fr: "plat", zh: "菜", pinyin: ["cai4"] },
+				{ fr: "sucre", zh: "糖", pinyin: ["tang2"] },
+				{ fr: "vinaigre", zh: "醋", pinyin: ["cu4"] },
+				{ fr: "poisson", zh: "鱼", pinyin: ["yu2"] },
+				{ fr: "acide", zh: "酸", pinyin: ["suan"] },
+				{ fr: "piquant", zh: "辣", pinyin: ["la4"] }/*,
+				// ---
+				{ fr: "soupe", zh: "汤", pinyin: ["tang1"] },
+				{ fr: "bœuf", zh: "牛肉", pinyin: ["niu2", "rou4"] },
+				{ fr: "cuire", zh: "烧", pinyin: ["shao1"] },
+				{ fr: "riz", zh: "米", pinyin: ["mi3"] },
+				{ fr: "ravioli chinois", zh: "水饺", pinyin: ["shui3", "jiao3"] },
+				{ fr: "attendre", zh: "等", pinyin: ["deng3"] },
+				{ fr: "est", zh: "东", pinyin: ["dong1"] },
+				{ fr: "ouest", zh: "西", pinyin: ["xi1"] },
+				{ fr: "magasin", zh: "商店", pinyin: ["shang1", "dian4"] },
+				{ fr: "vendre", zh: "卖", pinyin: ["mai4"] },
+				{ fr: "vêtements", zh: "衣服", pinyin: ["yi1", "fu"] }*/
+			]
+			
+		}
+	]
---
+++ hanziben.js
@@ -0,0 +1,133 @@
+function chineseCell(width, pinyin, characters) {
+	var table = $('<table class="chinese">');
+	for (var i=0; i<3; i++) {
+		var row = $('<tr class="pinyin">')
+		for (var j=0; j<width; j++) {
+			var td = $('<td colspan="2">');
+			if (i == 1) {
+				td.append("<span>" + pinyin[j] + "</span>");
+				td.addClass("pinyinText");
+			}		
+			row.append(td);
+		}
+		table.append(row);
+	}
+	for (var i=0; i<2;i++) {
+		if (i == 0)
+			var row = $('<tr class="hanzitop">');
+		else
+			var row = $('<tr class="hanzibottom">');
+		for (var j=0; j<width*2; j++) {
+			var cell = $('<td>');
+			if (i == 0 && j%2 == 0) {
+				var chineseCharacter = $('<div>');
+				chineseCharacter.addClass('character');
+				chineseCharacter.text(characters.substring(j/2, j/2 + 1));
+				cell.append(chineseCharacter);
+			}
+			row.append(cell);
+		}
+		table.append(row);
+	}
+		
+	return table;
+}
+	
+function generateQuizz(quizzSize, lessons) {
+	var allWords = [];
+	var quizz = [];
+	
+	// We list all the possible pinyins
+	hanziben.forEach(function(lesson) {
+		if(jQuery.inArray(lesson.lesson, lessons) > -1) {
+			lesson.words.forEach(function(word) {
+				allWords.push(word);
+			});
+		}
+	});
+	
+	if (quizzSize == 0)
+		quizzSize = allWords.length;
+	
+	while(quizzSize > 0 && allWords.length > 0) {
+		var idx = Math.floor(Math.random()*allWords.length);
+		quizz.push(allWords[idx]);
+		allWords.splice(idx, 1);
+		quizzSize--;
+	}
+	
+	return quizz;
+}
+
+function printQuizz(quizzSize, lessons, showPinyin, showCharacters) {
+	if (lessons.length == 0) {
+		alert("Tu as oublié de sélectionner une leçon!");
+		return;
+	}
+	$("#quizz").empty();
+	var quizz = generateQuizz(quizzSize, lessons);
+	quizz.forEach(function(word) {
+		
+		var quizzEntry = $('<table class="quizz">');
+		quizzEntry.append($('<tr>').append($('<td class="french">' + word.fr + '</td>')));
+		quizzEntry.append($('<tr>').append($('<td>').append(chineseCell(word.pinyin.length, word.pinyin, word.zh))));
+		
+		$("#quizz").append(quizzEntry);
+		
+	});
+	
+	$("#afterQuizz").show();
+	
+	if (showPinyin)
+		$(".pinyinText span").show();
+	else
+		$(".pinyinText span").hide();
+	
+	if (showCharacters)
+		$("div.character").show();
+	else
+		$("div.character").hide();
+		
+	// We set the page title
+	$("h2.title").text("Quizz Hanziben (leçon" + (lessons.length > 1 ? "s" : "") + " " + lessons.join(", ") + ")");
+}
+
+function generateLessonList() {
+	hanziben.forEach(function(lesson) {
+		var checked = lesson.checked ? 'checked="checked" ' : '';
+		$("#lessonsDiv").append($('<li><input type="checkbox" name="lesson[]" value="' + lesson.lesson + '" ' + checked + '/> ' + lesson.lesson + '</li>'));
+	});
+}
+
+function listLessons() {
+	var lessons = [];
+	$('#lessonsDiv :checked').each(function() {
+	  lessons.push($(this).val());
+	});
+	return lessons;
+}
+
+$(function() {
+	generateLessonList();
+	$("#generateButton").click(function() {
+		printQuizz($("#quizzSizeText option:selected").val(), listLessons(), $("#showPinyin:checked").length > 0, $("#showCharacters:checked").length > 0);
+	});
+	var all = true;
+	$("#checkAllNoneLink").click(function(e) {
+		$('input[name="lesson[]"]:checkbox').prop('checked',all);
+		all = !all;
+		e.preventDefault();
+	});
+	$("#showPinyin").click(function(e) {
+		if ($(this).is(':checked'))
+			$(".pinyinText span").show();
+		else
+			$(".pinyinText span").hide();
+	});
+	$("#showCharacters").click(function(e) {
+		if ($(this).is(':checked'))
+			$("div.character").show();
+		else
+			$("div.character").hide();
+	});
+});
